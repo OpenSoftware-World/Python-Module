9d06207933a4ba23e9b281599f061475e8b27459
OpenSoftware-World <umutarda863@gmail.com>
2026-07-15 17:32:06 +0300

    A critical bug fix for uninstall.sh.

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 read -p "Do you want to remove the Calc application by deleting it from the /usr/bin directory? " answer
 
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
-    sudo rm rf - /usr/bin/calc
+    sudo rm -f /usr/bin/calc
     echo "The Calc app has been successfully uninstalled."
 else
     echo "The uninstallation of the Calc app has been canceled."
